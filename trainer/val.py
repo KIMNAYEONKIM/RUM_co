38f8289a65277341a44b144ded1bc350823cfbb0
KIMNAYEON <rntk9413@gmail.com>
2025-04-01 15:20:55 +0900

    2차 수정

--- trainer/val.py
+++ trainer/val.py
@@ -148,6 +148,38 @@ def validate_withids(val_loader, model, criterion, args):
 
         print("valid_accuracy {top1.avg:.3f}".format(top1=top1))
     else:
+        for i, batch in enumerate(val_loader):
+            if len(batch) == 3:
+                image, target, unique_ids = batch
+            else:
+                image, target = batch
+                unique_ids = None
+                
+            image = image.cuda()
+            target = target.cuda()
+            
+            # compute output
+            with torch.no_grad():
+                output = model(image)
+                loss = criterion(output, target)
+                
+            output = output.float()
+            loss = loss.float()
+            
+            # measure accuracy and record loss
+            prec1 = utils.accuracy(output.data, target)[0]
+            losses.update(loss.item(), image.size(0))
+            top1.update(prec1.item(), image.size(0))
+            
+            if i % args.print_freq == 0:
+                print(
+                    "Test: [{0}/{1}]\t"
+                    "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
+                    "Accuracy {top1.val:.3f} ({top1.avg:.3f})".format(
+                        i, len(val_loader), loss=losses, top1=top1
+                    )
+                )
+        """
         for i, (image, target, unique_ids) in enumerate(val_loader):
 
             image = image.cuda()
@@ -179,5 +211,6 @@ def validate_withids(val_loader, model, criterion, args):
 
         # print("valid_accuracy {top1.avg:.3f}".format(top1=top1))
         # logger.log({"valid_accuracy": top1.avg})
+        """
 
     return top1.avg
